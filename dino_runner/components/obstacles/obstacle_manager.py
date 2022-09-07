262068367d389c3c0a3d49a6cb2082da693719ce
n3b8r <Ruben.Ipenza@fundacion-jala.org>
2022-09-07 00:03:39 -0400

    clase 2.1 dinosaurio
    ya funciona

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,15 +1,9 @@
-from random import choice
-from random import random
+
 import pygame
+import random
 
 from dino_runner.components.obstacles.cactus import Cactus_Small, Cactus_Large
 from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
-
-small_large_cactus = [
-    Cactus_Small(SMALL_CACTUS),
-    Cactus_Large(LARGE_CACTUS),
-]
-value = choice(small_large_cactus)
 
 class ObstacleManager:
 
@@ -18,7 +12,10 @@ class ObstacleManager:
 
     def update(self, game):
         if len(self.obstacles) == 0: 
-            self.obstacles.append.list(value)
+            if random.randint(0, 2) == 0:
+                self.obstacles.append(Cactus_Small(SMALL_CACTUS))
+            elif random.randint(0, 2) == 1:
+                self.obstacles.append(Cactus_Large(LARGE_CACTUS))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
