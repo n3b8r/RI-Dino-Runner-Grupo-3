f9c0c72f62ea6646bfe88f113b0e34d2e8c89e24
n3b8r <Ruben.Ipenza@fundacion-jala.org>
2022-09-07 21:44:18 -0400

    clase 3 dinosaurio
    código del trainer

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -3,13 +3,13 @@ import random
 from dino_runner.components.obstacles.obstacle import Obstacle
 
 
-class Cactus_Small(Obstacle):
+class SmallCactus(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
         self.rect.y = 325
 
-class Cactus_Large(Obstacle):    
+class LargeCactus(Obstacle):    
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,12 +1,9 @@
 import pygame
 import random
 
-#from random import choice
-from dino_runner.components.obstacles.cactus import Cactus_Small, Cactus_Large
+from dino_runner.components.obstacles.cactus import SmallCactus, LargeCactus
 from dino_runner.components.obstacles.birds import Bird
 from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
-#small_large = [Cactus_Small(SMALL_CACTUS), Cactus_Large(LARGE_CACTUS),]
-#qwe = choice(small_large)
 
 class ObstacleManager:
 
@@ -16,25 +13,24 @@ class ObstacleManager:
     def update(self, game):
         if len(self.obstacles) == 0:
             if random.randint(0, 2) == 0:
-                self.obstacles.append(Cactus_Small(SMALL_CACTUS))
+                self.obstacles.append(SmallCactus(SMALL_CACTUS))
             elif random.randint(0, 2) == 1:
-                self.obstacles.append(Cactus_Large(LARGE_CACTUS))
-            elif random.randint(0, 2) == 2:
+                self.obstacles.append(LargeCactus(LARGE_CACTUS))
+            else:
                 self.obstacles.append(Bird(BIRD))
-
-            #if len(self.obstacles) == 0:
-            #    self.obstacles.append(qwe) 
-            #elif len(self.obstacles) == 1:
-            #    self.obstacles.append(qwe) 
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break
 
 
     def draw(self, screen): 
         for obstacle in self.obstacles: 
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []
