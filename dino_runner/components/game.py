b92824886e1c2b54b1c546452bdfde92dfe6ebd6
n3b8r <Ruben.Ipenza@fundacion-jala.org>
2022-09-07 23:54:33 -0400

    clase 3 dinosaurio
    funciona!!!!!

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,7 +2,7 @@ import pygame
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
-from dino_runner.utils.constants import BG, FONT_STYLE, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import BG, BIRD, CLOUD, FONT_STYLE, ICON, LIVE_ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
 class Game:
@@ -13,7 +13,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
-        self.game_speed = 20
+        self.game_speed = 15
         self.x_pos_bg = 0
         self.y_pos_bg = 380
 
@@ -36,6 +36,7 @@ class Game:
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
+        self.score = 0
         self.obstacle_manager.reset_obstacles()
         while self.playing:
             self.events()
@@ -57,7 +58,7 @@ class Game:
     def update_score(self):
         self.score += 1
         if self.score % 100 == 0:
-            self.game_speed += 5
+            self.game_speed += 3
 
     def draw(self):
         self.clock.tick(FPS)
@@ -96,7 +97,7 @@ class Game:
 
     def show_menu(self):
         print(self.death_count)
-        self.screen.fill((255, 255, 255))
+        self.screen.fill((181, 178, 178))
         half_screen_height = SCREEN_HEIGHT // 2
         half_screen_width = SCREEN_WIDTH // 2
 
@@ -106,10 +107,40 @@ class Game:
             text_rect = text.get_rect()
             text_rect.center = (half_screen_width, half_screen_height)
             self.screen.blit(text, text_rect)
-        else: 
-            pass
+            
+            self.screen.blit(LIVE_ICON, (half_screen_width -20, half_screen_height - 140))
+            self.screen.blit(CLOUD, (half_screen_width -100, half_screen_height - 100))
+            self.screen.blit(CLOUD, (half_screen_width +60, half_screen_height - 100))  
+        else:
+            font = pygame.font.Font(FONT_STYLE, 50)
+            text = font.render("GAME OVER", True, (0, 0, 0))
+            text_rect = text.get_rect()
+            text_rect.center = (610, 80)
+            self.screen.blit(text, text_rect)
 
-        self.screen.blit(ICON, (half_screen_width -20, half_screen_height - 140))
+            font = pygame.font.Font(FONT_STYLE, 50)
+            text = font.render("PRESS ANY KEY TO REBOOT", True, (0, 0, 0))
+            text_rect = text.get_rect()
+            text_rect.center = (610, 520)
+            self.screen.blit(text, text_rect)
+            
+            font = pygame.font.Font(FONT_STYLE, 40)
+            text = font.render(F"Deaths: {self.death_count}", True, (0, 0, 0))
+            text_rect = text.get_rect()
+            text_rect.center = (600, 300)
+            self.screen.blit(text, text_rect)
 
+            text = font.render(F"Total score: {self.score}", True, (0, 0, 0))
+            text_rect = text.get_rect()
+            text_rect.center = (600, 400)
+            self.screen.blit(text, text_rect)
+
+            self.game_speed = 15
+            self.screen.blit(ICON, (half_screen_width -20, half_screen_height - 140))
+            self.screen.blit(CLOUD, (half_screen_width -100, half_screen_height - 100))
+            self.screen.blit(CLOUD, (half_screen_width +60, half_screen_height - 100))   
+
+             
+        
         pygame.display.update()
         self.handle_events_on_menu()
--- dino_runner/utils/constants.py
+++ dino_runner/utils/constants.py
@@ -10,6 +10,7 @@ IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 
 # Assets Constants
 ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png"))
+LIVE_ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoStart.png"))
 
 RUNNING = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1.png")),
